6065c3693e16662ef2bf30e659b4ccc90b54d522
ZhienCloud <woonzhien1@gmail.com>
2023-07-22 00:20:58 +0800

    fix css

--- todolist-frontend/src/components/NavigationToolbar.jsx
+++ todolist-frontend/src/components/NavigationToolbar.jsx
@@ -11,13 +11,14 @@ const NavigationToolbar = () => {
         src={logo}
         alt="To-Do List Logo"
       />
+      <h2 className="AppName">ToDoList</h2>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
        
             <li>
-              <Link to="/todolist/new">Create To Do List</Link>
+              <Link to="/todolist/new">Create ToDoList</Link>
             </li>
             {/* <li>
               <Link to="/profile">Profile</Link>
